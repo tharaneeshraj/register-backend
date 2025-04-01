e5cbe803c4e05424ae8e89c0ef1a3da1773462ce
Tharaneesh Raj <tharaneeshsami@gmail.com>
2025-04-01 12:43:52 +0530

    changes-commit

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-
-const express=require("express")
-const mongoose=require("mongoose")
-const dotenv = require("dotenv")
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
+// MongoDB Connection
 mongoose
-.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log("MongoDB Connection Error:", err))
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => {
+    console.log("MongoDB Connection Error:", err.message);
+    process.exit(1);
+  });
 
 
 
-app.use("/api/users", require("./routes/userroutes"))
+app.use("/api/users", require("./routes/userroutes"));
 
-
-const PORT =process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
